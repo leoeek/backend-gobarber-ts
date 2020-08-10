aad2894e2e4e6af827903e0d99e92c6d752f1b32
eek <leonardolp@gmail.com>
2020-08-10 14:13:19 -0300

    Adicionando teste para recuperaçao de senha

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,32 @@
+// import AppError from '@shared/erros/AppError';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'Leo Lima',
+      email: 'limalimao@gmail.com',
+      password: '123456',
+    });
+
+   await sendForgotPasswordEmail.execute({
+      email: 'limalimao@gmail.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+
+})
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,28 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/erros/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest ): Promise<void> {
+    const user = this.usersRepository.findByEmail(email);
+
+    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido.');
+  }
+}
+
+export default SendForgotPasswordEmailService;
